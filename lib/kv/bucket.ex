1a8fb9dfd025eff66937f4febb4c3dc08eae286e
Jingjing Duan <jduan@twitter.com>
2016-01-02 15:13:02 -0800

    Bucket.delete

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -4,7 +4,7 @@ defmodule KV.Bucket do
   Starts a new bucket.
   """
   def start_link do
-    {:ok, pid} = Agent.start_link(fn -> %{} end)
+    {:ok, _pid} = Agent.start_link(fn -> %{} end)
   end
 
   # implementation
@@ -20,5 +20,14 @@ defmodule KV.Bucket do
   """
   def put(pid, key, value) do
     Agent.update(pid, fn m -> Map.put(m, key, value) end)
+  end
+
+  @doc """
+  Delete a key from the bucket.
+
+  Return the current value of the key if key exists.
+  """
+  def delete(pid, key) do
+    Agent.get_and_update(pid, fn m -> Map.pop(m, key) end)
   end
 end
